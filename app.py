ad804a7b0af20db15806201839a65052687a2110
Dominic Frei <dominic.frei@mongodb.com>
2023-02-01 11:39:14 +0000

    Fix formatting

--- app.py
+++ app.py
@@ -19,12 +19,14 @@ collection: Collection = database.get_collection("books")
 # instantiating new object with "name"
 app: Flask = Flask(__name__)
 
+
 # our initial form page
 @app.route('/')  # root is "/"
 def index():
     return render_template("index.html")
 
-# CREATE and READ 
+
+# CREATE and READ
 @app.route('/books', methods=["GET", "POST"])
 def books():
     if request.method == 'POST':
@@ -46,13 +48,13 @@ def books():
             book = titles['book']
             pages = titles['pages']
             shelf = {'book': book, 'pages': pages}
-            novels.insert(0,shelf)
+            novels.insert(0, shelf)
 
         return novels
-    
+
 
 # UPDATE
-@app.route("/books/<string:book_id>", methods = ['PUT'])
+@app.route("/books/<string:book_id>", methods=['PUT'])
 def update_book(book_id: str):
     new_book: str = request.json['book']
     new_pages: str = request.json['pages']
@@ -60,11 +62,10 @@ def update_book(book_id: str):
 
     return f"UPDATE: Your book has been updated to: {new_book} ({new_pages} pages).\n"
 
+
 # DELETE
-@app.route("/books/<string:book_id>", methods = ['DELETE'])
+@app.route("/books/<string:book_id>", methods=['DELETE'])
 def remove_book(book_id: str):
     collection.delete_one({"_id": bson.ObjectId(book_id)})
 
     return f"DELETE: Your book (id = {book_id}) has been removed from your bookshelf.\n"
-
-
